7a36eb55d02123e1d2dacd289f77536a064dc815
matthewasher <m.asher@lancaster.ac.uk>
2023-12-21 11:04:11 +0000

    Main Classes are working as expected. Needs clean and comment.

--- cleaner_store.py
+++ cleaner_store.py
@@ -7,7 +7,7 @@
 import warnings, re
 from nltk.corpus import stopwords
 import pandas as pd
-
+import gensim
 
 class temprary():
 
@@ -164,12 +164,13 @@ class PhraseCleaner(PhraseStore):
         
     def valid_tuples(self):
         return[(self._word_list.loc[id,'Target'],self._word_list.loc[id,'Replace'])
-         for id in self._word_list.index()
+         for id in self._word_list.index
          if (self._word_list.loc[id,'Active'] and not self._word_list.loc[id,'Deleted'])]
 
     def clean_text(self, text):
         local = self.valid_tuples()
-        return wswaper.change_phrases(text,local)
+        sw = wswaper()
+        return sw.change_phrases(text, swaps=local)
 
 
 
@@ -226,16 +227,14 @@ class PhraseCleaner(PhraseStore):
         self._word_list = []
 """
 
-class Stopwords(PhraseCleaner):
-    def __init__(self, change_phrases=None):
-        super(Stopwords, self).__init__(change_phrases=change_phrases)
-
-
-
 class wswaper():
 
     @staticmethod
-    def change_phrase(text, swap):
+    def single_spaces(text):
+        return re.sub(' +', ' ', text)
+
+    @staticmethod
+    def change_phrase(text, swap, caps_sens=True, cleaned=True):
         # Swaps a single phrase (set of words) for another phrase.
         # Text: is either a string, or a list of words
         # target: is a phrase (ordered list of words) to be replaced in the text
@@ -251,54 +250,77 @@ class wswaper():
         if isinstance(target, str):
             target = [target]
 
+        if isinstance(target, tuple):
+            target = [word for word in target]
+
         if isinstance(replacement, str):
             replacement = [replacement]
+
+        if isinstance(replacement, tuple):
+            target = [word for word in replacement]
 
         # If the text is a single string, then a simple regex should work
         if isinstance(text, str):
             # Replace a string of the target words, separated by spaces, with a string of replacement words, separated
             # by spaces.
-            return re.sub(' '+str(' '.join(target).encode('unicode_escape') )+ ' ',
-                          ' '+str(' '.join(replacement).encode('unicode_escape')) + ' ',
-                          text) # TODO: Confirm this works with Escape charaters + CHECK REPLAC ' ' WITH variable length stop/ounctuation
-
-        # If the text is a list of words, treat all words as seprate
-        if len(target) == 1:
-            if len(replacement) == 1:
-                # If there is only a single word target and single word replacement, just go through each word and swap
-                return [word if word != target[0] else replacement[0] for word in text]
+            if caps_sens:
+                flags = re.IGNORECASE
             else:
-                # If there is a single target word, but an extended replacement phrase, accumulate updates
+                flags = re.NOFLAG
+
+            if 0:
+                out_text =  re.sub(str(' '.join(target).encode('unicode_escape')),
+                              str(' '.join(replacement).encode('unicode_escape')),
+                              text,flags = flags) # TODO: Confirm the problems with escapes.
+            else:
+                out_text = re.sub("{}".format(' '+' '.join(target)+' '),
+                                  "{}".format(' '+' '.join(replacement)+' '),
+                                  text,
+                                  flags=flags)
+            if cleaned:
+                out_text = wswaper.single_spaces(out_text)
+            return out_text
+
+        elif isinstance(text, list):
+            # If the text is a list of words, treat all words as seprate
+            if len(target) == 1:
+                if len(replacement) == 1:
+                    # If there is only a single word target and single word replacement, just go through each word and swap
+                    out_text =  [word if word != target[0] else replacement[0] for word in text]
+                else:
+                    # If there is a single target word, but an extended replacement phrase, accumulate updates
+                    out_text = []
+                    for word in text:
+                        if word == target[0]:
+                            out_text += replacement
+                        else:
+                            out_text.append(word)
+            else:
+                phrase_len = len(target)
                 out_text = []
-                for word in text:
-                    if word == target[0]:
-                        out_text += replacement
+                text_len = len(text)
+                i = 0
+                while i < text_len:
+                    if text[i] == target[0]:
+                        # If the current word in the text is the first in the target phrase, check if the following words
+                        # also match.
+                        sub_phrase = text[i:min(i + phrase_len, text_len)]
+                        if sub_phrase == target:
+                            # If they match, replace them all and skip to end of target phrase in input.
+                            out_text = out_text + replacement
+                            i += phrase_len
+                        else:
+                            # Else move onto next word
+                            out_text.append(text[i])
+                            i += 1
                     else:
-                        out_text.append(word)
-                return out_text
-        else:
-            phrase_len = len(target)
-            out_text = []
-            text_len = len(text)
-            i = 0
-            while i < text_len:
-                if text[i] == target[0]:
-                    # If the current word in the text is the first in the target phrase, check if the following words
-                    # also match.
-                    sub_phrase = text[i:min(i + phrase_len, text_len)]
-                    if sub_phrase == target:
-                        # If they match, replace them all and skip to end of target phrase in input.
-                        out_text = out_text + replacement
-                        i += phrase_len
-                    else:
-                        # Else move onto next word
+                        # else move onto next word
                         out_text.append(text[i])
                         i += 1
-                else:
-                    # else move onto next word
-                    out_text.append(text[i])
-                    i += 1
-            return out_text
+        if cleaned:
+            out_text = [word for word in out_text if word != '']
+
+        return out_text
 
     def change_phrases(self, text, swaps):
         for swap in swaps:
@@ -364,3 +386,48 @@ if __name__ == '__main__':
     if it is a tuple of lists, it is a swap word.
     """
 
+    test_text = "Introduction Big data (BD) is difficult to define. Precisely the features, scope, purview or threshold" \
+                " of BD continues to sow confusions and generate ethical controversies. Despite the preceding, the " \
+                "emerging consensus (Ekbia et al. 2015: p. 3; Alharthi et al. 2017: p. 286) is that BD is characterized" \
+                " by 5 Vs: Volume (that is, a vast amount of datasets requiring innovative and big tools for capturing, " \
+                "storing and analyzing); variety (un/semi/structured and collected from diverse sources);" \
+                " velocity (rapidly evolving datasets and expanded by actual data streams); " \
+                "veracity (that is, data uncertainty, quality, reliability or predictive force); " \
+                "and value (this is the artificial intelligence that is created either for learning new patterns " \
+                "in vast datasets or offering personalized services).There is a growing adoption of BD in various " \
+                "fields (engineering, life sciences, business, behavioural studies, online and offline commerce, " \
+                "education and politics). This article focuses on healthcare big data use and access. Healthcare " \
+                "big data (HBD) come from different sources such as sequencing data, Electronic Health Records " \
+                "(EHR), biological specimens, Quantified Self (QS), biomedical data, patient-reported data, " \
+                "biomarker data, medical imaging, large clinical trials; which may be stored in repositories or " \
+                "biobanks (Mittelstadt et al. 2016: p. 306). Other means of expanding HBD streams include healthcare " \
+                "literature databases like PubMed, automated sources like health and fitness devices and volunteered " \
+                "sources such as e-health networks like patientslikeme.HBD analytics integrates, explores, identifies" \
+                " clusters, correlates, analyze and infer (with an unparalleled degree of exactitude) based on datasets" \
+                " from the preceding complex heterogeneous sources to create – HBD – value or (artificial) intelligence" \
+                " for offering a range of personalized health services, support health policies or clinical decisions," \
+                " or advance science (Michael et al. 2013: p. 22). The HDB value relies mostly on analytic techniques" \
+                " such as algorithms and machine learning that are generated from processed data. HBD may be " \
+                "processed by using graphics processing units or cloud computing. The advancement in the omics " \
+                "studies, patient-contributed online data, imaging processes and the increasing affordability and " \
+                "accessibility of health electronic devices also imply that a large volume of heterogeneous data can " \
+                "now be analyzed to create HBD intelligence at a low cost. Advances have also been made in " \
+                "extracting previously difficult text data (from doctor’s notes, millions of books photographs " \
+                "from the past) for data analytics and mining purposes.Notwithstanding its (potential) benefits, HBD" \
+                " also creates a challenge for all stakeholders such as data utilizers, contributors and " \
+                "beneficiaries of HBD intelligence. New digital technologies can empower, yet they are also " \
+                "intrusive. HBD vividly exemplifies this dual character (Ekbia et al. 2015: p. 27). As an example, " \
+                "digital surveillance can usefully provide support for contact tracing in the event of a virus " \
+                "outbreak such as the COVID-19/Ebola outbreak. It could also monitor an individual’s healthcare " \
+                "decisions, behaviors or outcomes with the aim of fostering healthy habits and practices in the " \
+                "individual or the wider population, providing critical insights on health needs or status, " \
+                "assisting with the development of equitable interventions. However, it also introduces a level of " \
+                "oversight that significantly threatens individuals’ privacy.Furthermore, HBD may also be abused or " \
+                "misused. The literature is awash with examples of data misuse."
+
+    test_text = gensim.utils.simple_preprocess(str(test_text), deacc=True)
+
+    swaped = wordsets['swaps'](test_text)
+    joined = wordsets['joins'](swaped)
+    stoped = wordsets['stops'](joined)
+    print(stoped)
